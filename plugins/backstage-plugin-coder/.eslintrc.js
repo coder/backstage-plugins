ae85ee874c87590066e5b68876205fef4695cc77
Parkreiner <throwawayclover@gmail.com>
2024-02-13 21:31:15 +0000

    chore: disable eslint rules for scoping

--- plugins/backstage-plugin-coder/.eslintrc.js
+++ plugins/backstage-plugin-coder/.eslintrc.js
@@ -1,1 +1,10 @@
-module.exports = require('@backstage/cli/config/eslint-factory')(__dirname);
+const base = require('@backstage/cli/config/eslint-factory')(__dirname);
+
+module.exports = {
+  ...base,
+  rules: {
+    ...(base.rules ?? {}),
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'off',
+  },
+};
