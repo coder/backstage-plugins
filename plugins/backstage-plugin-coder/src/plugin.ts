67fa4cd320893c7b3e76934e1c38f9eede536285
Parkreiner <throwawayclover@gmail.com>
2024-02-14 14:23:59 +0000

    chore: add all public exports for plugin

--- plugins/backstage-plugin-coder/src/plugin.ts
+++ plugins/backstage-plugin-coder/src/plugin.ts
@@ -1,12 +1,8 @@
 import {
   createPlugin,
-  createRoutableExtension,
+  createComponentExtension,
 } from '@backstage/core-plugin-api';
 import { rootRouteRef } from './routes';
-
-export * from './components/CoderProvider';
-export * from './components/CoderErrorBoundary';
-export * from './components/CoderWorkspacesCard';
 
 export const coderPlugin = createPlugin({
   id: 'coder',
@@ -15,11 +11,55 @@ export const coderPlugin = createPlugin({
   },
 });
 
-export const CoderPage = coderPlugin.provide(
-  createRoutableExtension({
-    name: 'CoderPage',
-    component: () =>
-      import('./components/ExampleComponent').then(m => m.ExampleComponent),
-    mountPoint: rootRouteRef,
+/**
+ * All public component exports exposed by the plugin.
+ */
+export const CoderProvider = coderPlugin.provide(
+  createComponentExtension({
+    name: 'CoderAuthWrapper',
+    component: {
+      lazy: () =>
+        import('./components/CoderProvider').then(m => m.CoderProvider),
+    },
   }),
 );
+
+export const CoderAuthWrapper = coderPlugin.provide(
+  createComponentExtension({
+    name: 'CoderAuthWrapper',
+    component: {
+      lazy: () =>
+        import('./components/CoderAuthWrapper').then(m => m.CoderAuthWrapper),
+    },
+  }),
+);
+
+export const CoderErrorBoundary = coderPlugin.provide(
+  createComponentExtension({
+    name: 'CoderAuthWrapper',
+    component: {
+      lazy: () =>
+        import('./components/CoderErrorBoundary').then(
+          m => m.CoderErrorBoundary,
+        ),
+    },
+  }),
+);
+
+export const CoderWorkspacesCard = coderPlugin.provide(
+  createComponentExtension({
+    name: 'CoderAuthWrapper',
+    component: {
+      lazy: () =>
+        import('./components/CoderWorkspacesCard').then(
+          m => m.CoderWorkspacesCard,
+        ),
+    },
+  }),
+);
+
+/**
+ * All custom hooks exposed by the plugin.
+ */
+export { useCoderEntityConfig } from './hooks/useCoderEntityConfig';
+export { useCoderWorkspaces } from './hooks/useCoderWorkspaces';
