6ff277a000456f37b17bae91f0072bec86743f0a
Parkreiner <throwawayclover@gmail.com>
2024-03-23 20:15:34 +0000

    wip: reorganize test structure

--- plugins/backstage-plugin-coder/src/components/CoderAuthWrapper/CoderAuthWrapper.test.tsx
+++ plugins/backstage-plugin-coder/src/components/CoderAuthWrapper/CoderAuthWrapper.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { CoderProviderWithMockAuth } from '../../testHelpers/setup';
 import type { CoderAuthStatus } from '../CoderProvider';
 import { mockAppConfig } from '../../testHelpers/mockBackstageData';
@@ -34,42 +34,77 @@ function renderAuthWrapper({ dummyButtonText, authStatus }: RenderInputs) {
 }
 
 describe(`${CoderAuthWrapper.name}`, () => {
-  it('Displays the main children when the user is authenticated', async () => {
-    const buttonText = 'I have secret Coder content!';
-    renderAuthWrapper({
-      authStatus: 'authenticated',
-      dummyButtonText: buttonText,
+  describe('Displaying main content', () => {
+    it('Displays the main children when the user is authenticated', async () => {
+      const buttonText = 'I have secret Coder content!';
+      renderAuthWrapper({
+        authStatus: 'authenticated',
+        dummyButtonText: buttonText,
+      });
+
+      const button = await screen.findByRole('button', { name: buttonText });
+
+      // This assertion isn't necessary because findByRole will throw an error
+      // if the button can't be found within the expected period of time. Doing
+      // this purely to make the Backstage linter happy
+      expect(button).toBeInTheDocument();
+    });
+  });
+
+  describe('Loading UI', () => {
+    it('Is displayed while the auth is initializing', async () => {
+      const buttonText = "You shouldn't be able to see me!";
+      renderAuthWrapper({
+        authStatus: 'initializing',
+        dummyButtonText: buttonText,
+      });
+
+      await screen.findByText(/Loading/);
+      const button = screen.queryByRole('button', { name: buttonText });
+      expect(button).not.toBeInTheDocument();
+    });
+  });
+
+  describe('Token distrusted form', () => {
+    it("Is displayed when the user's auth status cannot be verified", async () => {
+      const buttonText = 'Not sure if you should be able to see me';
+      const distrustedTextMatcher = /Unable to verify token authenticity/;
+      const distrustedStatuses: readonly CoderAuthStatus[] = [
+        'distrusted',
+        'noInternetConnection',
+        'deploymentUnavailable',
+      ];
+
+      for (const status of distrustedStatuses) {
+        const { unmount } = await renderAuthWrapper({
+          authStatus: status,
+          dummyButtonText: buttonText,
+        });
+
+        await screen.findByText(distrustedTextMatcher);
+        const button = screen.queryByRole('button', { name: buttonText });
+        expect(button).not.toBeInTheDocument();
+
+        unmount();
+      }
     });
 
-    const button = await screen.findByRole('button', { name: buttonText });
-
-    // This assertion isn't necessary because findByRole will throw an error if
-    // the button can't be found within the expected period of time. Doing this
-    // purely to make the Backstage linter happy
-    expect(button).toBeInTheDocument();
-  });
-
-  it('Displays a loading UI element while the auth is initializing', async () => {
-    const buttonText = "You shouldn't be able to see me!";
-    renderAuthWrapper({
-      authStatus: 'initializing',
-      dummyButtonText: buttonText,
+    it('Lets the user eject the current token', async () => {
+      expect.hasAssertions();
     });
 
-    await screen.findByText('Loading');
-    const button = screen.queryByRole('button', { name: buttonText });
-    expect(button).not.toBeInTheDocument();
+    it('Will appear if auth status changes during re-renders', async () => {
+      expect.hasAssertions();
+    });
   });
 
-  it("Displays the 'token distrusted form' when the user's auth status cannot be verified", async () => {
-    expect.hasAssertions();
-  });
+  describe('Token submission form', () => {
+    it("Is displayed when the token either doesn't exist or is definitely not valid", async () => {
+      expect.hasAssertions();
+    });
 
-  it("Displays the token submission form when the token either doesn't exist or is definitely not valid", async () => {
-    expect.hasAssertions();
-  });
-
-  it("Submitting a new token through the form causes the component's children to appear", async () => {
-    expect.hasAssertions();
+    it("Submitting a new token causes the component's children to appear", async () => {
+      expect.hasAssertions();
+    });
   });
 });
