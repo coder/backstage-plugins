151bc69e0d4fc8eada1cb0aebe5a166334915691
Parkreiner <throwawayclover@gmail.com>
2024-03-25 19:49:43 +0000

    wip: add stubs for CoderWorkspacesCard tests

---
+++ plugins/backstage-plugin-coder/src/components/CoderWorkspacesCard/CoderWorkspacesCard.test.tsx
@@ -0,0 +1,8 @@
+/**
+ * @file This test serves as an integration test between all the different
+ * components within the CoderWorkspacesCard directory.
+ *
+ * This should remove the need to add any tests for Root
+ */
+import React from 'react';
+import { CoderWorkspacesCard } from './CoderWorkspacesCard';
