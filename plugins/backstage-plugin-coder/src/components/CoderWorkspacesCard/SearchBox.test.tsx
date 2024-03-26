b8ed45e5a2928c9e22ed1d67d915a6ca3e518c52
Parkreiner <throwawayclover@gmail.com>
2024-03-26 00:24:37 +0000

    chore: finish initial draft of tests for SearchBox

--- plugins/backstage-plugin-coder/src/components/CoderWorkspacesCard/SearchBox.test.tsx
+++ plugins/backstage-plugin-coder/src/components/CoderWorkspacesCard/SearchBox.test.tsx
@@ -3,19 +3,27 @@ import { renderInCoderEnvironment } from '../../testHelpers/setup';
 import { CardContext, WorkspacesCardContext } from './Root';
 import { SearchBox } from './SearchBox';
 import { mockCoderWorkspacesConfig } from '../../testHelpers/mockBackstageData';
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
-async function renderSearchBox() {
+type RenderInputs = Readonly<{
+  queryFilter?: string;
+}>;
+
+async function renderSearchBox(input?: RenderInputs) {
+  const { queryFilter = 'owner:me' } = input ?? {};
   const onFilterChange = jest.fn();
+
   const mockContext: WorkspacesCardContext = {
     onFilterChange,
-    queryFilter: 'owner:me',
+    queryFilter,
     headerId: "Doesn't matter",
     workspacesConfig: mockCoderWorkspacesConfig,
     workspacesQuery:
       null as unknown as WorkspacesCardContext['workspacesQuery'],
   };
 
-  const output = await renderInCoderEnvironment({
+  const renderOutput = await renderInCoderEnvironment({
     children: (
       <CardContext.Provider value={mockContext}>
         <SearchBox />
@@ -23,20 +31,88 @@ async function renderSearchBox() {
     ),
   });
 
-  return { ...output, onFilterChange };
+  const inputField = screen.getByRole<HTMLInputElement>('searchbox', {
+    name: /Search your Coder workspaces/i,
+  });
+
+  return { ...renderOutput, inputField, onFilterChange };
 }
 
 describe(`${SearchBox.name}`, () => {
+  describe('General functionality', () => {
+    const sampleInputText = 'Here is some cool text';
+
+    it('Will update the input immediately in response to the user typing', async () => {
+      const { inputField } = await renderSearchBox();
+      const user = userEvent.setup();
+
+      // Using triple-click to simulate highlighting all the text in the input
+      await user.tripleClick(inputField);
+      await user.keyboard(`[Backspace]${sampleInputText}`);
+
+      expect(inputField.value).toBe(sampleInputText);
+    });
+
+    it('Will debounce calls to the parent provider as the user types more characters', async () => {
+      const { inputField, onFilterChange } = await renderSearchBox();
+      const user = userEvent.setup();
+
+      await user.click(inputField);
+      await user.keyboard(sampleInputText);
+
+      expect(onFilterChange).not.toHaveBeenCalled();
+      await waitFor(() => expect(onFilterChange).toHaveBeenCalledTimes(1));
+    });
+  });
+
   /**
-   * Functionality to test:
-   * 1. Typing will immediately update the input
-   * 2. Typing does not immediately dispatch any updates to the Root callback
-   *    (update is eventually resolved via debouncing)
-   * 3. Clicking the clear button will:
-   *    1. Immediately clear out the visible input
-   *    2. Immediately clear out the query callback passed via root
-   *    3. Cancel any pending debounced calls
-   * 4. Clearing out the input field via keyboard is functionally equivalent
-   *    to clicking the clear button
+   * Two ways to clear the input:
+   * 1. Clicking the clear button
+   * 2. Hitting backspace on the keyboard until the input field is empty
+   *
+   * Which both immediately cause the following behavior when triggered:
+   * 1. Clears out the visible input
+   * 2. Calls the Root query callback with an empty string
+   * 3. Cancels any pending debounced calls
    */
+  describe('Text-clearing functionality', () => {
+    it('Lets the user clear the text via the Clear button', async () => {
+      const user = userEvent.setup();
+      const { inputField, onFilterChange } = await renderSearchBox({
+        queryFilter: '',
+      });
+
+      const clearButton = screen.getByRole('button', {
+        name: /Clear out search/i,
+      });
+
+      const sampleInputText = 'clear me out please';
+      await user.click(inputField);
+      await user.keyboard(sampleInputText);
+      expect(inputField.value).toBe(sampleInputText);
+      expect(onFilterChange).not.toHaveBeenCalled();
+
+      await user.click(clearButton);
+      expect(inputField.value).toBe('');
+      expect(onFilterChange).toHaveBeenCalledTimes(1);
+      expect(onFilterChange).toHaveBeenCalledWith('');
+    });
+
+    it('Lets the user trigger clear behavior by hitting Backspace', async () => {
+      const user = userEvent.setup();
+      const { inputField, onFilterChange } = await renderSearchBox({
+        queryFilter: 'H',
+      });
+
+      await user.click(inputField);
+      await user.keyboard('i');
+      expect(inputField.value).toBe('Hi');
+      expect(onFilterChange).not.toHaveBeenCalled();
+
+      await user.keyboard('[Backspace][Backspace]');
+      expect(inputField.value).toBe('');
+      expect(onFilterChange).toHaveBeenCalledTimes(1);
+      expect(onFilterChange).toHaveBeenCalledWith('');
+    });
+  });
 });
