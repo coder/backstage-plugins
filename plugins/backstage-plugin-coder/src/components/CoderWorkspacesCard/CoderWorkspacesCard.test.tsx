0b63640be4a09a42d576fcbd6c5f2300ca36a2a0
Parkreiner <throwawayclover@gmail.com>
2024-03-27 02:58:27 +0000

    fix: delete empty test file (to be added in future PR)

--- plugins/backstage-plugin-coder/src/components/CoderWorkspacesCard/CoderWorkspacesCard.test.tsx
+++
@@ -1,8 +0,0 @@
-/**
- * @file This test serves as an integration test between all the different
- * components within the CoderWorkspacesCard directory.
- *
- * This should remove the need to add any tests for Root
- */
-import React from 'react';
-import { CoderWorkspacesCard } from './CoderWorkspacesCard';
