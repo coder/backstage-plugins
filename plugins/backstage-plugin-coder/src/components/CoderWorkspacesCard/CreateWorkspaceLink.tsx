3cfc7c5bfbd02e0dee426ad8940b5baaac64dea6
Parkreiner <throwawayclover@gmail.com>
2024-04-01 15:33:53 +0000

    wip: commit progress on link update

--- plugins/backstage-plugin-coder/src/components/CoderWorkspacesCard/CreateWorkspaceLink.tsx
+++ plugins/backstage-plugin-coder/src/components/CoderWorkspacesCard/CreateWorkspaceLink.tsx
@@ -1,16 +1,22 @@
 import React, { type AnchorHTMLAttributes, type ForwardedRef } from 'react';
-import { makeStyles } from '@material-ui/core';
+import { type Theme, makeStyles } from '@material-ui/core';
 import { useWorkspacesCardContext } from './Root';
 
 import { VisuallyHidden } from '../VisuallyHidden';
 import AddIcon from '@material-ui/icons/AddCircleOutline';
 import Tooltip, { type TooltipProps } from '@material-ui/core/Tooltip';
 
-const useStyles = makeStyles(theme => {
+type StyleInput = Readonly<{
+  canCreateWorkspace: boolean;
+}>;
+
+type StyleKeys = 'root' | 'icon';
+
+const useStyles = makeStyles<Theme, StyleInput, StyleKeys>(theme => {
   const padding = theme.spacing(0.5);
 
   return {
-    root: {
+    root: ({ canCreateWorkspace }) => ({
       padding,
       width: theme.spacing(4) + padding,
       height: theme.spacing(4) + padding,
@@ -24,12 +30,14 @@ const useStyles = makeStyles(theme => {
       '&:hover': {
         backgroundColor: theme.palette.action.hover,
       },
-    },
+    }),
+
+    icon: {},
   };
 });
 
 type CreateButtonLinkProps = Readonly<
-  AnchorHTMLAttributes<HTMLAnchorElement> & {
+  Omit<AnchorHTMLAttributes<HTMLAnchorElement>, 'aria-disabled'> & {
     tooltipText?: string;
     tooltipProps?: Omit<TooltipProps, 'children' | 'title'>;
     tooltipRef?: ForwardedRef<unknown>;
@@ -45,22 +53,50 @@ export const CreateWorkspaceLink = ({
   tooltipProps = {},
   ...delegatedProps
 }: CreateButtonLinkProps) => {
-  const styles = useStyles();
   const { workspacesConfig } = useWorkspacesCardContext();
+  const canCreateWorkspace = Boolean(workspacesConfig.creationUrl);
+  const styles = useStyles({ canCreateWorkspace });
 
   return (
-    <Tooltip ref={tooltipRef} title={tooltipText} {...tooltipProps}>
+    <Tooltip
+      ref={tooltipRef}
+      title={
+        canCreateWorkspace ? tooltipText : 'Please add a template name value'
+      }
+      {...tooltipProps}
+    >
+      {/* eslint-disable-next-line jsx-a11y/no-redundant-roles --
+          Some browsers will render out <a> elements as having no role when the
+          href value is undefined. Need to make sure that the link role is
+          always defined, no matter what. The ESLint rule is wrong here. */}
       <a
+        role="link"
         target={target}
         className={`${styles.root} ${className ?? ''}`}
+        /**
+         * Also need to make sure that the link is correctly disabled when there
+         * is no href available.
+         * @see {@link https://www.scottohara.me/blog/2021/05/28/disabled-links.html}
+         */
         href={workspacesConfig.creationUrl}
+        aria-disabled={!canCreateWorkspace}
         {...delegatedProps}
       >
         {children ?? <AddIcon />}
 
         <VisuallyHidden>
-          {tooltipText}
-          {target === '_blank' && <> (Link opens in new tab)</>}
+          {canCreateWorkspace ? (
+            <>
+              {tooltipText}
+              {target === '_blank' && <> (Link opens in new tab)</>}
+            </>
+          ) : (
+            <>
+              This component does not have a usable template name. Please see
+              the disclosure section in this widget for steps on adding this
+              information.
+            </>
+          )}
         </VisuallyHidden>
       </a>
     </Tooltip>
