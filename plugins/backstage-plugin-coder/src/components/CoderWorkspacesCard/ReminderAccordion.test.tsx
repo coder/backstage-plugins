f841d1a929ae3dd32104d58edf9fa9cc9d6450e1
Parkreiner <throwawayclover@gmail.com>
2024-04-01 21:03:06 +0000

    wip: commit test stubs for ReminderAccordion

---
+++ plugins/backstage-plugin-coder/src/components/CoderWorkspacesCard/ReminderAccordion.test.tsx
@@ -0,0 +1,19 @@
+import { ReminderAccordion } from './ReminderAccordion';
+
+describe(`${ReminderAccordion.name}`, () => {
+  it('Lets the user open a single accordion item', async () => {
+    expect.hasAssertions();
+  });
+
+  it('Will close an open accordion item when that item is clicked', async () => {
+    expect.hasAssertions();
+  });
+
+  it('Will close any other open accordion items when a new item is clicked', async () => {
+    expect.hasAssertions();
+  });
+
+  it('Lets the user conditionally hide accordion items based on props', async () => {
+    expect.hasAssertions();
+  });
+});
