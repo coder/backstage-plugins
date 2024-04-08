e95fee5df44ac97c6ab9ab773afbc16b67b83604
Parkreiner <throwawayclover@gmail.com>
2024-04-08 23:08:22 +0000

    chore: finish tests for main snapshot manager class

--- plugins/backstage-plugin-coder/src/utils/StateSnapshotManager.test.ts
+++ plugins/backstage-plugin-coder/src/utils/StateSnapshotManager.test.ts
@@ -1,14 +1,9 @@
-import { ReadonlyJsonValue } from '../typesConstants';
-import { StateSnapshotManager } from './StateSnapshotManager';
+import type { ReadonlyJsonValue } from '../typesConstants';
+import {
+  StateSnapshotManager,
+  defaultDidSnapshotsChange,
+} from './StateSnapshotManager';
 
-/**
- * Qualities I want to test for:
- * 1. Lets another system subscribe and unsubscribe to internal snapshot changes
- * 2. Will use a shallow-ish comparison algorithm if no custom method is provided
- * 3. Allows for a custom comparison algorithm to be defined during instantiation
- * 4. Rejects new snapshots that are equivalent to old ones, and does NOT notify subscriptions
- * 5. Will yield the current snapshot as a 100% immutable value when getSnapshot is called
- */
 describe(`${StateSnapshotManager.name}`, () => {
   it('Lets external systems subscribe and unsubscribe to internal snapshot changes', () => {
     type SampleData = Readonly<{
@@ -18,23 +13,104 @@ describe(`${StateSnapshotManager.name}`, () => {
 
     const sampleData = [
       { snapshotA: false, snapshotB: true },
+      { snapshotA: 0, snapshotB: 1 },
       { snapshotA: 'cat', snapshotB: 'dog' },
+      { snapshotA: null, snapshotB: 'neat' },
       { snapshotA: {}, snapshotB: { different: true } },
+      { snapshotA: [], snapshotB: ['I have a value now!'] },
     ] as const satisfies readonly SampleData[];
 
     for (const { snapshotA, snapshotB } of sampleData) {
       const subscriptionCallback = jest.fn();
       const manager = new StateSnapshotManager({
         initialSnapshot: snapshotA,
+        didSnapshotsChange: defaultDidSnapshotsChange,
       });
 
       const unsubscribe = manager.subscribe(subscriptionCallback);
       manager.updateSnapshot(snapshotB);
       expect(subscriptionCallback).toHaveBeenCalledTimes(1);
+      expect(subscriptionCallback).toHaveBeenCalledWith(snapshotB);
 
       unsubscribe();
       manager.updateSnapshot(snapshotA);
       expect(subscriptionCallback).toHaveBeenCalledTimes(1);
     }
   });
+
+  it('Lets user define custom comparison algorithm during instantiation', () => {
+    type SampleData = Readonly<{
+      snapshotA: ReadonlyJsonValue;
+      snapshotB: ReadonlyJsonValue;
+      compare: (A: ReadonlyJsonValue, B: ReadonlyJsonValue) => boolean;
+    }>;
+
+    const exampleDeeplyNestedJson: ReadonlyJsonValue = {
+      value1: {
+        value2: {
+          value3: {
+            value4: [{ valueX: true }, { valueY: false }],
+          },
+        },
+      },
+    };
+
+    const sampleData = [
+      {
+        snapshotA: exampleDeeplyNestedJson,
+        snapshotB: JSON.parse(JSON.stringify(exampleDeeplyNestedJson)),
+        compare: (A, B) => JSON.stringify(A) === JSON.stringify(B),
+      },
+      {
+        snapshotA: { tag: 'snapshot', value: 1 },
+        snapshotB: { tag: 'snapshot', value: 9999 },
+        compare: (A, B) => {
+          const recastA = A as Record<string, unknown>;
+          const recastB = B as Record<string, unknown>;
+          return recastA.tag === recastB.tag;
+        },
+      },
+    ] as const satisfies readonly SampleData[];
+
+    for (const { snapshotA, snapshotB, compare } of sampleData) {
+      const subscriptionCallback = jest.fn();
+      const manager = new StateSnapshotManager({
+        initialSnapshot: snapshotA,
+        didSnapshotsChange: compare,
+      });
+
+      void manager.subscribe(subscriptionCallback);
+      manager.updateSnapshot(snapshotB);
+      expect(subscriptionCallback).toHaveBeenCalledWith(snapshotB);
+    }
+  });
+
+  it('Rejects new snapshots that are equivalent to old ones, and does NOT notify subscriptions', () => {
+    type SampleData = Readonly<{
+      snapshotA: ReadonlyJsonValue;
+      snapshotB: ReadonlyJsonValue;
+    }>;
+
+    const sampleData = [
+      { snapshotA: true, snapshotB: true },
+      { snapshotA: 'kitty', snapshotB: 'kitty' },
+      { snapshotA: null, snapshotB: null },
+      { snapshotA: [], snapshotB: [] },
+      { snapshotA: {}, snapshotB: {} },
+    ] as const satisfies readonly SampleData[];
+
+    for (const { snapshotA, snapshotB } of sampleData) {
+      const subscriptionCallback = jest.fn();
+      const manager = new StateSnapshotManager({
+        initialSnapshot: snapshotA,
+        didSnapshotsChange: defaultDidSnapshotsChange,
+      });
+
+      void manager.subscribe(subscriptionCallback);
+      manager.updateSnapshot(snapshotB);
+      expect(subscriptionCallback).not.toHaveBeenCalled();
+    }
+  });
 });
+
+describe(`${defaultDidSnapshotsChange.name}`, () => {});
--- plugins/backstage-plugin-coder/src/utils/StateSnapshotManager.ts
+++ plugins/backstage-plugin-coder/src/utils/StateSnapshotManager.ts
@@ -38,6 +38,67 @@ interface SnapshotManagerApi<TSnapshot extends ReadonlyJsonValue> {
   updateSnapshot: (newSnapshot: TSnapshot) => void;
 }
 
+function areSameByReference(v1: unknown, v2: unknown) {
+  // Comparison looks wonky, but Object.is handles more edge cases than ===
+  // for these kinds of comparisons, but it itself has an edge case
+  // with -0 and +0. Still need === to handle that comparison
+  return Object.is(v1, v2) || (v1 === 0 && v2 === 0);
+}
+
+/**
+ * Favors shallow-ish comparisons (will check one level deep for objects and
+ * arrays, but no more)
+ */
+export function defaultDidSnapshotsChange<TSnapshot extends ReadonlyJsonValue>(
+  oldSnapshot: TSnapshot,
+  newSnapshot: TSnapshot,
+): boolean {
+  if (areSameByReference(oldSnapshot, newSnapshot)) {
+    return false;
+  }
+
+  const oldIsPrimitive =
+    typeof oldSnapshot !== 'object' || oldSnapshot === null;
+  const newIsPrimitive =
+    typeof newSnapshot !== 'object' || newSnapshot === null;
+
+  if (oldIsPrimitive && newIsPrimitive && oldSnapshot !== newSnapshot) {
+    return true;
+  }
+
+  const changedFromObjectToPrimitive = !oldIsPrimitive && newIsPrimitive;
+  const changedFromPrimitiveToObject = oldIsPrimitive && !newIsPrimitive;
+
+  if (changedFromObjectToPrimitive || changedFromPrimitiveToObject) {
+    return true;
+  }
+
+  if (Array.isArray(oldSnapshot) && Array.isArray(newSnapshot)) {
+    const sameByShallowComparison =
+      oldSnapshot.length === newSnapshot.length &&
+      oldSnapshot.every((element, index) =>
+        areSameByReference(element, newSnapshot[index]),
+      );
+
+    return !sameByShallowComparison;
+  }
+
+  const oldInnerValues: unknown[] = Object.values(oldSnapshot as Object);
+  const newInnerValues: unknown[] = Object.values(newSnapshot as Object);
+
+  if (oldInnerValues.length !== newInnerValues.length) {
+    return true;
+  }
+
+  for (const [index, value] of oldInnerValues.entries()) {
+    if (value !== newInnerValues[index]) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
 export class StateSnapshotManager<
   TSnapshot extends ReadonlyJsonValue = ReadonlyJsonValue,
 > implements SnapshotManagerApi<TSnapshot>
@@ -51,75 +112,12 @@ export class StateSnapshotManager<
 
     this.subscriptions = new Set();
     this.activeSnapshot = initialSnapshot;
-    this.didSnapshotsChange =
-      didSnapshotsChange ?? this.defaultDidSnapshotsChange;
+    this.didSnapshotsChange = didSnapshotsChange ?? defaultDidSnapshotsChange;
   }
 
   private notifySubscriptions(): void {
     const snapshotBinding = this.activeSnapshot;
     this.subscriptions.forEach(cb => cb(snapshotBinding));
-  }
-
-  private static areSameByReference<TSnapshot extends ReadonlyJsonValue>(
-    snapshot1: TSnapshot | undefined,
-    snapshot2: TSnapshot | undefined,
-  ): boolean {
-    // Comparison looks wonky, but Object.is handles more edge cases than ===
-    // for these kinds of comparisons, but it itself has an edge case
-    // with -0 and +0. Still need === to handle that comparison
-    return (
-      Object.is(snapshot1, snapshot2) || (snapshot1 === 0 && snapshot2 === 0)
-    );
-  }
-
-  /**
-   * Favors shallow-ish comparisons (will check one level deep for objects and
-   * arrays)
-   */
-  private defaultDidSnapshotsChange(
-    oldSnapshot: TSnapshot,
-    newSnapshot: TSnapshot,
-  ): boolean {
-    if (StateSnapshotManager.areSameByReference(oldSnapshot, newSnapshot)) {
-      return false;
-    }
-
-    const changedFromObjectToPrimitive =
-      typeof oldSnapshot === 'object' &&
-      (typeof newSnapshot !== 'object' || newSnapshot === null);
-
-    const changedFromPrimitiveToObject =
-      (typeof oldSnapshot !== 'object' || oldSnapshot === null) &&
-      typeof newSnapshot === 'object';
-
-    if (changedFromObjectToPrimitive || changedFromPrimitiveToObject) {
-      return true;
-    }
-
-    if (Array.isArray(oldSnapshot) && Array.isArray(newSnapshot)) {
-      const sameByShallowComparison =
-        oldSnapshot.length === newSnapshot.length &&
-        oldSnapshot.every((element, index) =>
-          StateSnapshotManager.areSameByReference(element, newSnapshot[index]),
-        );
-
-      return !sameByShallowComparison;
-    }
-
-    const oldInnerValues: unknown[] = Object.values(oldSnapshot as Object);
-    const newInnerValues: unknown[] = Object.values(newSnapshot as Object);
-
-    if (oldInnerValues.length !== newInnerValues.length) {
-      return true;
-    }
-
-    for (const [index, value] of oldInnerValues.entries()) {
-      if (value !== newInnerValues[index]) {
-        return true;
-      }
-    }
-
-    return false;
   }
 
   unsubscribe = (callback: SubscriptionCallback<TSnapshot>): void => {
