13ba1ffeee18e65aeb18cd332a4d8caf317836a5
Parkreiner <throwawayclover@gmail.com>
2024-04-10 18:54:37 +0000

    refactor: update BackstateHttpError to be aware of Axios

--- plugins/backstage-plugin-coder/src/api/errors.ts
+++ plugins/backstage-plugin-coder/src/api/errors.ts
@@ -1,11 +1,13 @@
+import type { AxiosResponse } from 'axios';
+
 /**
  * Makes it easier to expose HTTP responses in the event of errors and also
  * gives TypeScript a faster way to type-narrow on those errors
  */
 export class BackstageHttpError extends Error {
-  #failedResponse: Response;
+  #failedResponse: AxiosResponse;
 
-  constructor(errorMessage: string, failedResponse: Response) {
+  constructor(errorMessage: string, failedResponse: AxiosResponse) {
     super(errorMessage);
     this.name = 'BackstageHttpError';
     this.#failedResponse = failedResponse;
@@ -20,10 +22,11 @@ export class BackstageHttpError extends Error {
   }
 
   get ok() {
-    return this.#failedResponse.ok;
+    const status = this.#failedResponse.status;
+    return !(status >= 200 && status <= 299);
   }
 
   get contentType() {
-    return this.#failedResponse.headers.get('content_type');
+    return this.#failedResponse.headers['Content-Type'];
   }
 }
