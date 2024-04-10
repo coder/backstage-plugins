ac3bbf62eb3de6ca5b30ea7b08dc065186817bd8
Parkreiner <throwawayclover@gmail.com>
2024-04-10 16:35:03 +0000

    wip: commit progress on CoderClient update

--- plugins/backstage-plugin-coder/src/api/CoderClient.ts
+++ plugins/backstage-plugin-coder/src/api/CoderClient.ts
@@ -1,152 +1,132 @@
 import axios from 'axios';
-import * as coderApi from 'coder/site/src/api/api';
+import * as coderSdkApi from 'coder/site/src/api/api';
+
 import { getErrorMessage } from 'coder/site/src/api/errors';
 import type { Workspace } from 'coder/site/src/api/typesGenerated';
 
-import { parse } from 'valibot';
 import type { CoderWorkspacesConfig } from '../hooks/useCoderWorkspacesConfig';
 import { BackstageHttpError } from './errors';
-import {
-  type ReadonlyJsonValue,
-  // type Workspace,
-  type WorkspaceBuildParameter,
-  workspaceBuildParametersSchema,
-  workspacesResponseSchema,
-} from '../typesConstants';
 import { DiscoveryApi, createApiRef } from '@backstage/core-plugin-api';
 import { CoderAuthApi } from './Auth';
 import { CODER_API_REF_ID_PREFIX } from '../typesConstants';
 
 type CoderClientConfigOptions = Readonly<{
   apiRoutePrefix: string;
+  authHeaderKey: string;
   assetsRoutePrefix: string;
   requestTimeoutMs: number;
 }>;
 
 export const defaultCoderClientConfigOptions = {
   apiRoutePrefix: '/coder/api/v2',
-  assetsRoutePrefix: '/coder', // Intentionally left as single slash
+  assetsRoutePrefix: '/coder',
+  authHeaderKey: 'Coder-Session-Token',
   requestTimeoutMs: 20_000,
 } as const satisfies CoderClientConfigOptions;
-
-export type ArbitraryApiCallFunctionConfig = Readonly<{
-  endpoint: string;
-  body: ReadonlyJsonValue;
-
-  // Type definition is a TypeScript hack; the overall type definition collapses
-  // down to type string, but the other methods specified still show up in
-  // auto-complete
-  method?: 'GET' | 'POST' | 'PUT' | 'DELETE' | (string & {});
-}>;
 
 export type ApiEndpoints = Readonly<{
   apiRoute: string;
   assetsRoute: string;
 }>;
 
-export interface CoderClientApi {
-  readonly apiEndpoints: ApiEndpoints;
-  getWorkspaces: (coderQuery: string) => Promise<readonly Workspace[]>;
+type ApiNamespace = Readonly<
+  typeof coderSdkApi & {
+    getWorkspacesByRepo: (
+      coderQuery: string,
+      config: CoderWorkspacesConfig,
+    ) => Promise<readonly Workspace[]>;
+  }
+>;
 
-  getWorkspacesByRepo: (
-    coderQuery: string,
-    config: CoderWorkspacesConfig,
-  ) => Promise<readonly Workspace[]>;
-
-  unsafeApiCall: (
-    config: ArbitraryApiCallFunctionConfig,
-  ) => Promise<ReadonlyJsonValue>;
-}
+export type CoderClientApi = Readonly<{
+  api: ApiNamespace;
+  isAuthValid: boolean;
+  validateAuth: () => Promise<boolean>;
+  getApiEndpoints: () => ApiEndpoints;
+}>;
 
 export class CoderClient implements CoderClientApi {
   private readonly discoveryApi: DiscoveryApi;
   private readonly authApi: CoderAuthApi;
   private readonly options: CoderClientConfigOptions;
-  public readonly api = coderApi;
+  private latestBaseEndpoint: string;
 
-  // Spotify recommends using the DiscoveryApi over the ConfigApi nowadays, but
-  // the tradeoff is that the method for getting the proxy endpoints is async.
-  // Caching the latest value from each call to ensure that some kind of value
-  // is available synchronously for UI updates. Make sure that a dummy proxy
-  // call is made as part of the constructor
-  private latestProxyEndpoint: string = '';
+  readonly api: ApiNamespace;
 
   constructor(
     discoveryApi: DiscoveryApi,
     authApi: CoderAuthApi,
     options?: Partial<CoderClientConfigOptions>,
   ) {
+    // Instantiate config properties via constructor's function signature
     this.discoveryApi = discoveryApi;
     this.authApi = authApi;
+    this.latestBaseEndpoint = '';
+    this.options = { ...defaultCoderClientConfigOptions, ...(options ?? {}) };
+
+    // Wire up API namespace and patch in additional function for end-user
+    // convenience. Cannot inline function definitions inside this.api
+    // assignment because of funky JavaScript `this` rules
+    const getWorkspacesByRepo: typeof this.getWorkspacesByRepo = (
+      coderQuery,
+      config,
+    ) => this.getWorkspacesByRepo(coderQuery, config);
+
+    this.api = { ...coderSdkApi, getWorkspacesByRepo };
+
+    // Wire up Backstage APIs to be aware of Axios, and keep it aware of the
+    // most up-to-date state
     axios.interceptors.request.use(async config => {
       config.baseURL = await this.getBaseProxyEndpoint();
-      // TODO: this is not right, needs to return token string
-      config.headers['Coder-Session-Token'] = await this.getRequestInit();
+      config.headers[this.options.authHeaderKey] = this.authApi.token;
       return config;
     });
-    this.options = { ...defaultCoderClientConfigOptions, ...(options ?? {}) };
+
+    // Call DiscoveryApi to populate initial endpoint path, so that the path
+    // can be accessed synchronously from the UI
+    void this.getBaseProxyEndpoint();
   }
 
+  get isAuthValid(): boolean {
+    return this.authApi.isTokenValid;
+  }
+
+  // Backstage officially recommends that you use the DiscoveryApi over the
+  // ConfigApi nowadays, and that you call it before each request. But the
+  // problem is that the Discovery API has no synchronous methods for getting
+  // endpoints, meaning that there's no great built-in way to access that data
+  // synchronously. Have to cache it over time
   private async getBaseProxyEndpoint(): Promise<string> {
-    const latestPath = await this.discoveryApi.getBaseUrl('proxy');
-    this.latestProxyEndpoint = latestPath;
-    return latestPath;
+    const latestBase = await this.discoveryApi.getBaseUrl('proxy');
+    this.latestBaseEndpoint = latestBase;
+    return latestBase;
   }
 
-  private async getApiEndpoint(): Promise<string> {
-    const baseEndpoint = await this.getBaseProxyEndpoint();
-    return `${baseEndpoint}${this.options.apiRoutePrefix}`;
-  }
+  private async getWorkspacesByRepo(
+    coderQuery: string,
+    config: CoderWorkspacesConfig,
+  ): Promise<readonly Workspace[]> {}
 
-  private async getAssetsEndpoint(): Promise<string> {
-    const baseEndpoint = await this.getBaseProxyEndpoint();
-    return `${baseEndpoint}${this.options.assetsRoutePrefix}`;
-  }
+  /* ***************************************************************************
+   * All public functions should be defined as arrow functions to ensure they
+   * can be passed around React without risk of losing their "this" context
+   ****************************************************************************/
 
-  private async getRequestInit(): Promise<RequestInit> {
-    await this.authApi.assertAuthIsValid();
-    const authInit = this.authApi.getRequestInit();
+  validateAuth = (): Promise<boolean> => {
+    return this.authApi.validateAuth(async () => {
+      // try {
+      //    await this.api.getUserLoginType();
+      //   return true;
+      // } catch (err) {
+      // }
+      // if (response.status >= 400 && response.status !== 401) {
+      //   throw new BackstageHttpError('Failed to complete request', response);
+      // }
+    });
+  };
 
-    return {
-      ...authInit,
-      signal: AbortSignal.timeout(this.options.requestTimeoutMs),
-    };
-  }
-
-  // This is private because it is an implementation detail of
-  // getWorkspacesByRepo. Can likely be removed once that function is updated to
-  // account for the new API endpoint
-  private async getWorkspaceBuildParameters(
-    workspaceBuildId: string,
-  ): Promise<readonly WorkspaceBuildParameter[]> {
-    const apiEndpoint = await this.getApiEndpoint();
-    const requestInit = await this.getRequestInit();
-
-    const res = await fetch(
-      `${apiEndpoint}/workspacebuilds/${workspaceBuildId}/parameters`,
-      requestInit,
-    );
-
-    if (!res.ok) {
-      throw new BackstageHttpError(
-        `Failed to retreive build params for workspace ID ${workspaceBuildId}`,
-        res,
-      );
-    }
-
-    if (!res.headers.get('content-type')?.includes('application/json')) {
-      throw new BackstageHttpError(
-        '200 request has no data - potential proxy issue',
-        res,
-      );
-    }
-
-    const json = await res.json();
-    return parse(workspaceBuildParametersSchema, json);
-  }
-
-  get apiEndpoints(): ApiEndpoints {
-    if (!this.latestProxyEndpoint) {
+  getApiEndpoints = (): ApiEndpoints => {
+    if (!this.latestBaseEndpoint) {
       return {
         apiRoute: '',
         assetsRoute: '',
@@ -155,123 +135,9 @@ export class CoderClient implements CoderClientApi {
 
     const { apiRoutePrefix, assetsRoutePrefix } = this.options;
     return {
-      apiRoute: `${this.latestProxyEndpoint}${apiRoutePrefix}`,
-      assetsRoute: `${this.latestProxyEndpoint}${assetsRoutePrefix}`,
+      apiRoute: `${this.latestBaseEndpoint}${apiRoutePrefix}`,
+      assetsRoute: `${this.latestBaseEndpoint}${assetsRoutePrefix}`,
     };
-  }
-
-  /* ***************************************************************************
-   * All public functions should be defined as arrow functions to ensure they
-   * can be passed around React without risk of losing their "this" context
-   ****************************************************************************/
-
-  getWorkspaces = async (coderQuery: string): Promise<readonly Workspace[]> => {
-    // const apiEndpoint = await this.getApiEndpoint();
-    // const requestInit = await this.getRequestInit();
-    // const urlParams = new URLSearchParams({ q: coderQuery, limit: '0' });
-
-    // const res = await fetch(
-    //   `${apiEndpoint}/workspaces?${urlParams.toString()}`,
-    //   requestInit,
-    // );
-
-    // if (!res.ok) {
-    //   throw new BackstageHttpError(
-    //     `Unable to retrieve workspaces for query (${coderQuery})`,
-    //     res,
-    //   );
-    // }
-
-    // if (!res.headers.get('content-type')?.includes('application/json')) {
-    //   throw new BackstageHttpError(
-    //     '200 request has no data - potential proxy issue',
-    //     res,
-    //   );
-    // }
-
-    // const json = await res.json();
-    // const { workspaces } = parse(workspacesResponseSchema, json);
-    // const assetsEndpoint = await this.getAssetsEndpoint();
-
-    const response = await this.api.getWorkspaces({ q: coderQuery });
-
-    const withRemappedImgUrls = response.workspaces.map(ws => {
-      const templateIcon = ws.template_icon;
-      if (!templateIcon.startsWith('/')) {
-        return ws;
-      }
-
-      return { ...ws, template_icon: `${assetsEndpoint}${templateIcon}` };
-    });
-
-    return withRemappedImgUrls;
-  };
-
-  getWorkspacesByRepo = async (
-    coderQuery: string,
-    config: CoderWorkspacesConfig,
-  ): Promise<readonly Workspace[]> => {
-    const response = await this.api.getWorkspaces({ q: coderQuery });
-    const paramResults = await Promise.allSettled(
-      response.workspaces.map(ws =>
-        this.getWorkspaceBuildParameters(ws.latest_build.id),
-      ),
-    );
-
-    const matchedWorkspaces: Workspace[] = [];
-    for (const [index, res] of paramResults.entries()) {
-      if (res.status === 'rejected') {
-        continue;
-      }
-
-      for (const param of res.value) {
-        const include =
-          config.repoUrlParamKeys.includes(param.name) &&
-          param.value === config.repoUrl;
-
-        if (include) {
-          // Doing type assertion just in case noUncheckedIndexedAccess compiler
-          // setting ever gets turned on; this shouldn't ever break, but it's
-          // technically not type-safe
-          matchedWorkspaces.push(workspaces[index] as Workspace);
-          break;
-        }
-      }
-    }
-
-    return matchedWorkspaces;
-  };
-
-  unsafeApiCall = async <TReturn = any>(
-    config: ArbitraryApiCallFunctionConfig,
-  ): Promise<TReturn> => {
-    const { endpoint, body, method = 'GET' } = config;
-
-    const baseRequestInit = this.getRequestInit();
-    const requestInit: RequestInit = {
-      ...baseRequestInit,
-      method,
-      body: JSON.stringify(body),
-    };
-
-    const res = await fetch(endpoint, requestInit);
-
-    if (!res.ok) {
-      throw new BackstageHttpError(
-        `Unable to complete ${method} request for ${endpoint}`,
-        res,
-      );
-    }
-
-    if (!res.headers.get('content-type')?.includes('application/json')) {
-      throw new BackstageHttpError(
-        '200 request has no data - potential proxy issue',
-        res,
-      );
-    }
-
-    const json = await res.json();
-    return json;
   };
 }
 
