cd519a6366461e2250eebd4cab74e5f5faf1ab16
Parkreiner <throwawayclover@gmail.com>
2024-04-10 15:10:27 +0000

    refactor: update type definitions for core Auth definition

--- plugins/backstage-plugin-coder/src/api/Auth.ts
+++ plugins/backstage-plugin-coder/src/api/Auth.ts
@@ -1,17 +1,39 @@
 /**
- * @file This is a very, very tiny file for the moment; expect it to get bigger
- * once we add support for Coder OAuth
+ * @file Defines shared values and types used among any custom Coder auth
+ * implementations for the frontend.
  */
 import { createApiRef } from '@backstage/core-plugin-api';
 import { CODER_API_REF_ID_PREFIX } from '../typesConstants';
 
+export type IsAuthValidCallback = (token: string) => boolean | Promise<boolean>;
+
+type AuthSubscriptionPayload = Readonly<{
+  token: string;
+  isTokenValid: boolean;
+}>;
+
+export type AuthSubscriptionCallback<
+  TSubscriptionPayload extends AuthSubscriptionPayload = AuthSubscriptionPayload,
+> = (payload: TSubscriptionPayload) => void;
+
 /**
  * Shared set of properties among all Coder auth implementations
  */
-export interface CoderAuthApi {
-  assertAuthIsValid: () => Promise<void>;
-  getRequestInit: () => RequestInit;
-}
+export type CoderAuthApi<
+  TPayload extends AuthSubscriptionPayload = AuthSubscriptionPayload,
+> = TPayload & {
+  clearToken: () => void;
+  registerNewToken: (newToken: string) => void;
+  validateAuth: (validationMethod: IsAuthValidCallback) => Promise<boolean>;
+
+  /**
+   * Returns an pre-wired unsubscribe callback to remove fuss of needing to hold
+   * onto the original callback if it's not directly needed anymore
+   */
+  subscribe: (callback: AuthSubscriptionCallback<TPayload>) => () => void;
+  unsubscribe: (callback: AuthSubscriptionCallback<TPayload>) => void;
+  getStateSnapshot: () => AuthSubscriptionPayload;
+};
 
 /**
  * A single, shared auth API ref that can be used with any of the CoderAuth
