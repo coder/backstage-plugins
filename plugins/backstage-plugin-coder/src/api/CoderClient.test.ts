239661da0fcee45daba8680b366c37e9970335d6
Parkreiner <throwawayclover@gmail.com>
2024-04-29 15:07:20 +0000

    wip: add all test stubs for CoderClient

--- plugins/backstage-plugin-coder/src/api/CoderClient.test.ts
+++ plugins/backstage-plugin-coder/src/api/CoderClient.test.ts
@@ -25,7 +25,19 @@ function getConstructorApis(): ConstructorApis {
 }
 
 describe(`${CoderClient.name}`, () => {
-  describe('syncToken functionality', () => {});
+  describe('syncToken functionality', () => {
+    it('Will load the provided token into the client if it is valid', async () => {
+      expect.hasAssertions();
+    });
+
+    it('Will NOT load the provided token into the client if it is invalid', async () => {
+      expect.hasAssertions();
+    });
+
+    it('Will propagate any other error types to the caller', async () => {
+      expect.hasAssertions();
+    });
+  });
 
   describe('cleanupClient functionality', () => {
     it('Will prevent any new SDK requests from going through', async () => {
@@ -33,9 +45,9 @@ describe(`${CoderClient.name}`, () => {
       client.cleanupClient();
 
       // Request should fail, even though token is valid
-      await expect(() => client.syncToken(mockCoderAuthToken)).rejects.toThrow(
-        disabledClientError,
-      );
+      await expect(() => {
+        return client.syncToken(mockCoderAuthToken);
+      }).rejects.toThrow(disabledClientError);
 
       await expect(() => {
         return client.sdk.getWorkspaces({
@@ -44,9 +56,22 @@ describe(`${CoderClient.name}`, () => {
         });
       }).rejects.toThrow(disabledClientError);
     });
+
+    it('Will abort any pending requests', async () => {
+      expect.hasAssertions();
+    });
   });
 
-  describe('Making fetch requests in general', () => {});
+  // Eventually the Coder SDK is going to get too big to test every single
+  // function. Focus tests on the functionality specifically being patched in
+  // for Backstage
+  describe('Coder SDK', () => {
+    it('Will remap all workspace icon URLs to use the proxy URL if necessary', async () => {
+      expect.hasAssertions();
+    });
 
-  describe('Coder SDK', () => {});
+    it('Lets the user search for workspaces by repo URL', async () => {
+      expect.hasAssertions();
+    });
+  });
 });
