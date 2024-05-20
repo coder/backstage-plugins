f097f9b1b3f78fe32d6e8e0c2803260d496bb3ba
Parkreiner <throwawayclover@gmail.com>
2024-05-20 17:03:19 +0000

    wip: commit progress on modal

--- plugins/backstage-plugin-coder/src/components/CoderAuthFormDialog/CoderAuthFormDialog.tsx
+++ plugins/backstage-plugin-coder/src/components/CoderAuthFormDialog/CoderAuthFormDialog.tsx
@@ -1,5 +1,11 @@
-import { Dialog, makeStyles } from '@material-ui/core';
-import React, { type PropsWithChildren, useState } from 'react';
+import React, { type HTMLAttributes, useState } from 'react';
+import { useId } from '../../hooks/hookPolyfills';
+import { makeStyles } from '@material-ui/core';
+import { LinkButton } from '@backstage/core-components';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogActions from '@material-ui/core/DialogActions';
 
 const useStyles = makeStyles(theme => ({
   trigger: {
@@ -20,19 +26,43 @@ const useStyles = makeStyles(theme => ({
     },
   },
 
-  dialog: {
-    zIndex: 9999,
+  dialogContainer: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexFlow: 'column nowrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  dialogPaper: {
+    width: '100%',
+  },
+
+  contentContainer: {
+    padding: `0 ${theme.spacing(3)}px ${theme.spacing(4)}px`,
+  },
+
+  actionsRow: {
+    display: 'flex',
+    flexFlow: 'row nowrap',
+    justifyContent: 'center',
+    padding: `${theme.spacing(1)}px ${theme.spacing(2)}px`,
+  },
+
+  closeButton: {
+    padding: `${theme.spacing(0.5)}px ${theme.spacing(1)}px`,
+    color: theme.palette.primary.main,
   },
 }));
 
 type DialogProps = Readonly<
-  PropsWithChildren<{
+  Omit<HTMLAttributes<HTMLButtonElement>, 'onClick' | 'className'> & {
     open?: boolean;
     onOpen?: () => void;
     onClose?: () => void;
     triggerClassName?: string;
-    dialogClassName?: string;
-  }>
+  }
 >;
 
 export function CoderAuthFormDialog({
@@ -40,12 +70,20 @@ export function CoderAuthFormDialog({
   onOpen,
   onClose,
   triggerClassName,
-  dialogClassName,
   open: outerIsOpen,
 }: DialogProps) {
+  const hookId = useId();
   const styles = useStyles();
   const [innerIsOpen, setInnerIsOpen] = useState(false);
+
+  const handleClose = () => {
+    setInnerIsOpen(false);
+    onClose?.();
+  };
+
   const isOpen = outerIsOpen ?? innerIsOpen;
+  const titleId = `${hookId}-dialog-title`;
+  const descriptionId = `${hookId}-dialog-description`;
 
   return (
     <>
@@ -61,13 +99,28 @@ export function CoderAuthFormDialog({
 
       <Dialog
         open={isOpen}
-        className={`${styles.dialog} ${dialogClassName ?? ''}`}
-        onClose={() => {
-          setInnerIsOpen(false);
-          onClose?.();
+        onClose={handleClose}
+        aria-labelledby={titleId}
+        aria-describedby={descriptionId}
+        classes={{
+          container: styles.dialogContainer,
+          paper: styles.dialogPaper,
         }}
       >
-        Blah
+        <DialogTitle id={titleId}>Authenticate with Coder</DialogTitle>
+        <DialogContent className={styles.contentContainer}>
+          Here's some other content
+        </DialogContent>
+
+        <DialogActions className={styles.actionsRow}>
+          <LinkButton
+            to=""
+            onClick={handleClose}
+            className={styles.closeButton}
+          >
+            <span>Close</span>
+          </LinkButton>
+        </DialogActions>
       </Dialog>
     </>
   );
